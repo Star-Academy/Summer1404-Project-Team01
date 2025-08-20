20a4eaf8ccaabbe1dab5b879b98fa916c1ed5d33
aslan-es <a.eskandarix@gmail.com>
2025-08-20 15:55:23 +0330

    feat(pages): create initial welcome

--- web/ETL/src/app/app.config.ts
+++ web/ETL/src/app/app.config.ts
@@ -1,50 +1,13 @@
-import { APP_INITIALIZER, ApplicationConfig, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
-
-function initializeKeycloak(keycloak: KeycloakService, platformId: object) {
-  if (isPlatformBrowser(platformId)) {
-    return () =>
-      keycloak.init({
-        config: {
-          url: 'https://185.60.136.56',
-          realm: 'team1',
-          clientId: 'frontend'
-        },
-        initOptions: {
-          onLoad: 'check-sso',
-          silentCheckSsoRedirectUri:
-            window.location.origin + '/assets/silent-check-sso.html'
-        }
-      });
-  }
-  return () => Promise.resolve();
-}
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptorsFromDi()),
-    KeycloakService,
-
-
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService, PLATFORM_ID]
-    },
-
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: KeycloakBearerInterceptor,
-      multi: true
-    }
+    provideHttpClient()
   ]
 };
---
+++ web/ETL/src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,29 @@
+import {Component, inject} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-pages',
+  standalone:true,
+  imports: [],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+  private http = inject(HttpClient);
+
+  public login(): void{
+    const backEndLoginUrl = 'http://localhost:8080';
+
+    this.http.get<{ loginUrl: string }>(backEndLoginUrl).subscribe({
+      next:(response) => {
+        window.location.href = response.loginUrl;
+      },
+      error:(err) => {
+        console.error('Could not get login URL from backend', err);
+      }
+    })
+  }
+
+}
