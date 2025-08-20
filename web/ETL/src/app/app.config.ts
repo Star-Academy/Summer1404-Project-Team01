b99ee7db63e72936ea90af84b6804d832319a828
aslan-es <a.eskandarix@gmail.com>
2025-08-20 10:23:55 +0330

    feat(auth): integrate Keycloak for authentication flow

--- web/ETL/src/app/app.config.ts
+++ web/ETL/src/app/app.config.ts
@@ -1,9 +1,50 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { KeycloakBearerInterceptor, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService, platformId: object) {
+  if (isPlatformBrowser(platformId)) {
+    return () =>
+      keycloak.init({
+        config: {
+          url: 'https://185.60.136.56',
+          realm: 'team1',
+          clientId: 'frontend'
+        },
+        initOptions: {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+            window.location.origin + '/assets/silent-check-sso.html'
+        }
+      });
+  }
+  return () => Promise.resolve();
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptorsFromDi()),
+    KeycloakService,
+
+
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService, PLATFORM_ID]
+    },
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: KeycloakBearerInterceptor,
+      multi: true
+    }
+  ]
 };
